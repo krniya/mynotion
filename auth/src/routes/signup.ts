9d2682354b751dfd25f7681de5c4d3b5c3d1382f
Nitish Kumar <krnitish@live.in>
2023-12-10 18:03:43 +0530

    Error handling & MongoDB configured for Auth service - Custom error handling added - not found error configured - Auth mongodb deployment & service configured - User model created - Signup functionality added - configured password hashing - Custom error - Bad request configured

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,23 +1,46 @@
 import express, {Request, Response} from 'express'
-import { body, validationResult } from 'express-validator'
+import { body } from 'express-validator'
+import { User } from '../models/users'
+import { BadRequestError } from '../errors/bad-request-error'
+import jwt from 'jsonwebtoken'
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
+
+// * @desc        User Sign up
+// * @route       POST /api/users/signup
+// * @access      Public
 router.post('/api/users/signup', [
+    body('name').notEmpty().withMessage('Please provide your name'),
     body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().isLength({min:8, max: 20}).withMessage('Password must be between 8 and 20 characters')
-], (req: Request, res: Response) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        throw new Error('Invalid email or password')
+    body('password').trim().isLength({min:8, max: 20}).withMessage('Password must be between 8 and 20 characters'),
+    body('dob').notEmpty().withMessage('Please provide date in proper format'),
+    body('gender').notEmpty().withMessage('Please provide proper gender (M/F/O)')
+], validateRequest, async (req: Request, res: Response) => {
+    const {name, email, password, dob, gender} = req.body
+    //* Checking if email is already been used
+    const existingUser = await User.findOne({ email })
+    if(existingUser) {
+        throw new BadRequestError('Email already in use')
+    }
+    //* User Sign Up
+    const user = User.build({name, email, password, dob, gender})
+    await user.save();
+
+    //* Generate JWT
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!)
+
+    //* Storing the JWT on session
+    req.session = {
+        jwt: userJwt
     }
 
-    const {email, password} = req.body
+    res.status(201).send(user);
 
-    console.log('creating a user...')
-    throw new Error('Error connecting to Database')
-
-    res.send({})
 })
 
 export {router as signupRouter}
